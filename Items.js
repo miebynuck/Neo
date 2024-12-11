2940cd2c72176e146d2f4fdf98a23c92098f4feb
miebynuck <miebynuck@gmail.com>
2024-12-11 15:28:18 -0300

    Create Items.js

---
+++ Items.js
@@ -0,0 +1,21 @@
+// Listado de posibles premios en el gacha
+export const items = {
+    pets: [
+        { name: 'Biscuit kougra', img: 'assets/Biscuit Kougra.png', type: 'Pet', LVL: 1, Books: 0, Nickname: '' },
+        { name: 'Blue kougra', img: 'assets/Blue Kougra.png', type: 'Pet', LVL: 1, Books: 0, Nickname: ''},
+        { name: 'Baby Kougra', img: 'assets/Baby kougra.png', type: 'Pet', LVL: 1, Books: 0, Nickname: ''}
+    ],
+    comida: [
+        { name: 'Jugo de Pera', img: 'assets/Jugo de pera.gif', type: 'comida'  },
+        { name: 'Comida para Gato', img: 'assets/Biscuit Kougra.png', type: 'comida' },
+        { name: 'Comida para Gato', img: 'assets/Biscuit Kougra.png', type: 'comida' }
+    ],
+    libros: [
+        { name: 'Libro de Entrenamiento' , img: 'assets/Jugo de pera.gif', type: 'libro' },
+        { name: 'Guía de Mascotas', img: 'assets/Jugo de pera.gif', type: 'libro' }
+    ],
+    fondos: [
+        { name: 'Fondo de Bosque',  img: 'assets/Jugo de pera.gif', type: 'fondo' },
+        { name: 'Fondo de Ciudad', img: 'assets/Jugo de pera.gif', type: 'fondo' }
+    ]
+};
